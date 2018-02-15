4843194d780cc631826b4cd7fcd598e083340b6d
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-02-15 21:07:27 +0000

    automatically backed up by learn

---
+++ lib/create.sql
@@ -0,0 +1,14 @@
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY, 
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date TEXT,
+  end_date TEXT
+);
+
+CREATE TABLE users (
+  id PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+);
